b532993d16f5fdeccad664ff94d3d20574ab17a1
KMutasim <kmutasim@insightintechnology.com>
2021-11-25 15:52:04 +0600

    Creating Entities

---
+++ Core/Entities/Registration.cs
@@ -0,0 +1,26 @@
+﻿using Core.Contracts;
+using System;
+
+namespace Core.Entities
+{
+    public class Registration : BaseEntity, ITenant
+    {
+        public Registration(string name, string email, DateTime dateOfBirth)
+        {
+            Name = name;
+            Email = email;  
+            DateOfBirth = dateOfBirth;
+        }
+
+        protected Registration()
+        {
+
+        }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TenantId { get; set; }
+
+    }
+}
